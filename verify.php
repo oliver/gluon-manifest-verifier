afeade2803c77c3a621cf71426978e082067a545
Oliver Gerlich <oliver.gerlich@gmx.de>
2018-01-28 20:03:53 +0100

    add GPL v3 license

--- verify.php
+++ verify.php
@@ -1,5 +1,24 @@
 <?php
 
+/*
+    Gluon Manifest Verifier - verifies and displays signatures of Gluon manifest file.
+    Copyright (C) 2018  Oliver Gerlich
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 $configArray = parse_ini_file("gluon-manifest-verifier.ini", true);
 $MANIFEST_BASE = $configArray["general"]["base_url"];
 if (!$MANIFEST_BASE)
